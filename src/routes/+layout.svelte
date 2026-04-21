b7dbf30cb4c30d830b5b792f6391bd1e6bc68c2b
ꜱʏᴛʜ <git@sythron.gay>
2026-04-22 01:12:53 +1000

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import favicon from "$lib/assets/favicon.svg";
+	import { title } from "node:process";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Syth</title>
+</svelte:head>
+
+{@render children()}
